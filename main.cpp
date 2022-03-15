734087fba73805b3020edb49c053a6f5c1b3b399
SienPeerlinck <100282748+SienPeerlinck@users.noreply.github.com>
2022-03-15 13:45:13 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
 #include <arduino.h>
 #include <iostream>
 
+const int ledPin = 12; // pin the LED is attached to
+int incomingByte;      // variable stores  serial data
+
 void setup() {
-	SerialUSB.begin(9600);
+  // initialize serial communication:
+  Serial.begin(9600);
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
 }
+
 void loop() {
-	if(SerialUSB.available() > 0) {
-		char data = SerialUSB.read();
-		char str[2];
-		str[0] = data;
-		str[1] = '\0';
-		SerialUSB.print(str);
-	}
+  // see if there's incoming serial data:
+  if (SerialUSB.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    incomingByte = SerialUSB.read();
+    // if it's a capital H (ASCII 72), turn on the LED:
+    if (incomingByte == 'H') {
+      digitalWrite(ledPin, HIGH);
+    }
+    // if it's an L (ASCII 76) turn off the LED:
+    if (incomingByte == 'L') {
+      digitalWrite(ledPin, LOW);
+    }
+  }
 }
