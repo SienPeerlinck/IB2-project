8a88e44400823ea1fb1309b4462f1d491e51b608
SienPeerlinck <100282748+SienPeerlinck@users.noreply.github.com>
2022-03-15 13:41:00 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,28 +1,15 @@
-#include <Arduino.h>
-#include "Segmentdriver.h"
+#include <arduino.h>
+#include <iostream>
 
-Segmentdriver seg1(0x3A);
-Segmentdriver seg2(0x39);
-
-uint8_t var1 = 0;
-uint8_t var2 = 0;
-
-void setup(){
-  seg1.init();
-  seg2.init();
+void setup() {
+	SerialUSB.begin(9600);
 }
-
-void loop(){
-  seg1.write_value(var1);
-  seg2.write_value(var2);
-  delay(500);
-  var2++;
-  if (var2 > 9){
-    var2=0;
-    var1++;
-    if (var1 > 9){
-      var1=0;
-      var2=0;
-    }
-  }
+void loop() {
+	if(SerialUSB.available() > 0) {
+		char data = SerialUSB.read();
+		char str[2];
+		str[0] = data;
+		str[1] = '\0';
+		SerialUSB.print(str);
+	}
 }
